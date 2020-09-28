0e293da3f7a905c03256bae441082d1ecf964a6b
omm26 <omm26@cornell.edu>
2020-09-28 18:05:23 -0400

    Section 4 work commit

--- sort-test.c
+++ sort-test.c
@@ -5,7 +5,7 @@
 
 void sort( int* x_ptr, int* y_ptr )
 {
-  if ( (*x_ptr) < (*y_ptr) ) {
+  if ( (*x_ptr) > (*y_ptr) ) {
     int temp = *x_ptr;
     *x_ptr   = *y_ptr;
     *y_ptr   = temp;
@@ -21,6 +21,12 @@ void test_case_1_basic()
   sort( &a, &b );
   ECE2400_CHECK_INT_EQ( a, 5 );
   ECE2400_CHECK_INT_EQ( b, 9 );
+
+  a = 10;
+  b = 2;
+  sort( &a, &b );
+  ECE2400_CHECK_INT_EQ( a, 2 );
+  ECE2400_CHECK_INT_EQ( b, 10 );
 }
 
 int main( int argc, char* argv[] )
